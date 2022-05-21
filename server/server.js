bd3de57ed7364fb7e78a7061dc9b30da0329eb88
Nayan Bagale <nvbagale@yahoo.com>
2022-05-21 20:25:55 +0530

    fun

--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
     res.json({ "Name": "Nayan" })
 })
 
+app.get('/:word', (req, res) => {
+    console.log(req.params.word)
+    res.redirect('/')
+})
+
 
 
 app.listen(5000, () => {
